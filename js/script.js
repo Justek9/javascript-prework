a8faa5518f7b0e2ad9e255436c2405131ba6acf4
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-05-03 20:04:25 +0200

    set players move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Computer's move
+
 let randomNumber = Math.floor(Math.random() * 3 + 1)
 
 console.log('Wylosowana liczba to: ' + randomNumber)
@@ -16,4 +18,27 @@ function setComputerMove() {
 
 setComputerMove(randomNumber)
 // console.log(computerMove)
-printMessage('Mój ruch to: ' + computerMove)
+// printMessage('Mój ruch to: ' + computerMove)
+
+// Player's move
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
+
+console.log('Gracz wpisał: ' + playerInput)
+
+let playerMove = 'nieznany ruch'
+
+function setPlayersMove() {
+	if (playerInput == '1') {
+		playerMove = 'kamień'
+	} else if (playerInput == '2') {
+		playerMove = 'papier'
+	} else if (playerInput == '3') {
+		playerMove = 'nożyce'
+	}
+}
+
+setPlayersMove()
+console.log(playerMove);
+
+// printMessage('Twój ruch to: ' + playerMove);
