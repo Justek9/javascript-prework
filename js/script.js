dc7f2aa3c14861d693fa6deaa710383e8dc943b4
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-05-03 19:55:57 +0200

    generate random number, set computer move

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-function printMessage(msg) {
-	let div = document.createElement('div')
-	div.innerHTML = msg
-	document.getElementById('messages').appendChild(div)
+let randomNumber = Math.floor(Math.random() * 3 + 1)
+
+console.log('Wylosowana liczba to: ' + randomNumber)
+
+let computerMove = 'nieznany ruch'
+
+function setComputerMove() {
+	if (randomNumber == 1) {
+		computerMove = 'kamień'
+	} else if (randomNumber == 2) {
+		computerMove = 'papier'
+	} else if (randomNumber == 3) {
+		computerMove = 'nożyce'
+	}
 }
 
-function clearMessages() {
-	document.getElementById('messages').innerHTML = ''
-}
-
-let computerMove = `kamień`
-let playerMove = 'papier'
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!')
+setComputerMove(randomNumber)
+// console.log(computerMove)
+printMessage('Mój ruch to: ' + computerMove)
